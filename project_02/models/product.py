7869cd583514911b8c473b14ebda57aa81a2c6a6
HaihengDev <haihenglim9@gmail.com>
2026-06-07 09:23:26 +0700

    Add initial product model, schemas, database connection, and router

---
+++ project_02/models/product.py
@@ -0,0 +1,11 @@
+from bson import ObjectId
+
+class Product:
+    @staticmethod
+    def to_dict(product):
+        return {
+            "id": str(product["_id"]),
+            "name": product["name"],
+            "stock": product["stock"],
+            "price": product['price']
+        }
